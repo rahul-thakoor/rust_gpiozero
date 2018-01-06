49937a83e3923c04f8946813a67798d7966ac157
Rahul Thakoor <rahul.thakoor@gmail.com>
2018-01-06 11:24:06 +0000

    Led on/off working

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#![crate_type = "lib"]
+#![crate_name = "gpiozero"]
+
+extern crate sysfs_gpio;
+
+use sysfs_gpio::*;
+
+pub mod led;
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+extern crate gpiozero;
+
+use std::thread::sleep;
+use std::time::Duration;
+
+use gpiozero::led::Led;
+
+fn main() {
+    let led = Led::new(17);
+
+   loop {
+            led.on();
+            sleep(Duration::from_millis(1500));
+            led.off();
+            sleep(Duration::from_millis(1500));
+   }
+}
