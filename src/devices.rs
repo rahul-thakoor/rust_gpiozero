5a411d76a65555db28e7b4f950fe7dc0c21b1d9e
rahul-thakoor <rahul.thakoor@gmail.com>
2018-01-30 16:56:21 +0400

    export modified

--- src/devices.rs
+++ src/devices.rs
@@ -11,20 +11,18 @@ impl GPIODevice {
         //Create a new Pin with the provided pin_num
         let mut gpio = Pin::new(pin_num);
          //check if pin is not already exported
-        if gpio.is_exported(){
-            println!("Gpio {} is not available", gpio.get_pin());
-        }else{
-            //try to export the selected pin
-            match gpio.export() {
-                Ok(()) => println!("Gpio {} exported!", gpio.get_pin()),
-                Err(err) => println!("Gpio {} could not be exported: {}", gpio.get_pin(), err),
-            }
-            return GPIODevice {pin:gpio}
+       
+        //try to export the selected pin
+        match gpio.pin.export() {
+            Ok(()) => println!("Gpio {} exported!", gpio.get_pin()),
+            Err(err) => println!("Gpio {} could not be exported: {}", gpio.get_pin(), err),
+        
         }
+        GPIODevice {pin:gpio}
         
     }
 
-    pub close(&self){
+    pub fn close(&self){
         if self.pin.is_exported(){
             match self.pin.unexport() {
             Ok(()) => println!("Gpio {} unexported!", gpio.get_pin()),
