09f08b97ecc50e90c130121c235be554acf12587
rahul-thakoor <rahul.thakoor@gmail.com>
2019-02-27 09:48:54 +0400

    Fix formatting

--- examples/blink.rs
+++ examples/blink.rs
@@ -6,5 +6,5 @@ fn main() {
     // Create a new LED attached to Pin 17
     let mut led = LED::new(17);
     // blink the LED
-    led.blink(2, 3);
+    led.blink(2.0, 3.0);
 }
--- examples/softpwm.rs
+++ examples/softpwm.rs
@@ -7,7 +7,8 @@ fn main() {
     let mut led = PWMLED::new(17);
 
     // blink the LED 5 times
-    led.blink(2.0, 2.0, 1.0, 1.0, Some(3));
+    led.set_blink_count(5);
+    led.blink(2.0, 2.0, 1.0, 1.0);
 
     // wait for key press to exit
     let _ = io::stdin().read(&mut [0u8]).unwrap();
