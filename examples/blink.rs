8f3bdc718eade45acf2eb62794e897f6c326b5a6
rahul-thakoor <rahul.thakoor@gmail.com>
2019-02-28 18:22:34 +0400

    Fix blink example

--- examples/blink.rs
+++ examples/blink.rs
@@ -5,6 +5,10 @@ use rust_gpiozero::*;
 fn main() {
     // Create a new LED attached to Pin 17
     let mut led = LED::new(17);
-    // blink the LED
-    led.blink(2.0, 3.0);
+
+    // on_time = 2 secs, off_time=3 secs
+    led.blink(2.0,3.0);
+
+    // prevent program from exiting immediately
+    led.wait();
 }
