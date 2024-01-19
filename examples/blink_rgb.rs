dd5e20d739b2057c5e20532e207f07993c1c94cb
David Llewellyn-Jones <david@flypig.co.uk>
2024-01-19 17:07:04 +0000

    Update RGBLED digital implementation

--- examples/blink_rgb.rs
+++ examples/blink_rgb.rs
@@ -1,18 +1,27 @@
 //! Blinks an LED : on_time: 2 seconds and off_time: 3 seconds
 
 use rust_gpiozero::*;
+use palette::rgb::Rgb;
+use std::{thread, time};
 
 fn main() {
     // Create a new LED attached to Pin 17
-    let mut led = RGBLED::new(23, 24, 25);
+    let mut led = RGBLED::new(12, 19, 13);
 
-    // on_time = 2 secs, off_time=3 secs
-    led.red.blink(0.11, 0.05);
-    led.green.blink(0.7, 0.17);
-    led.blue.blink(0.23, 0.03);
+    led.set_color(Rgb::new(1.0, 1.0, 0.0));
+    thread::sleep(time::Duration::from_millis(500));
+    led.set_color(Rgb::new(0.0, 1.0, 0.0));
+    thread::sleep(time::Duration::from_millis(500));
+    led.set_color(Rgb::new(0.0, 1.0, 1.0));
+    thread::sleep(time::Duration::from_millis(500));
 
-    // prevent program from exiting immediately
-    led.red.wait();
-    led.green.wait();
-    led.blue.wait();
+//    // on_time = 2 secs, off_time=3 secs
+//    led.red.blink(0.11, 0.05);
+//    led.green.blink(0.7, 0.17);
+//    led.blue.blink(0.23, 0.03);
+
+//    // prevent program from exiting immediately
+//    led.red.wait();
+//    led.green.wait();
+//    led.blue.wait();
 }
